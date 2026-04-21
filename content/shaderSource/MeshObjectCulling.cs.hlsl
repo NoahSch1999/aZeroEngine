be6506455eef7446f3c260060a2b5185b4f06233
NoahSch1999 <77342033+NoahSch1999@users.noreply.github.com>
2026-04-21 20:09:26 +0200

    Added object and meshlet culling indirect drawing to make the entire renderpipeline gpu driven.

---
+++ content/shaderSource/MeshObjectCulling.cs.hlsl
@@ -0,0 +1,47 @@
+#include "MeshletCommon.hlsli"
+#include "MeshDrawPassCommon.hlsli"
+
+struct MeshObjectCullingConstants
+{
+    uint NumMeshObjects;
+    uint IndirectArgumentMeshCullingBuffer;
+    uint PassedMeshesCounterBuffer;
+};
+
+ConstantBuffer<BindingConstants> Bindings : register(b0);
+
+ConstantBuffer<MeshObjectCullingConstants> PassConstants : register(b1);
+
+[numthreads(THREADS_PER_X, 1, 1)]
+void main(uint3 dtid : SV_DispatchThreadID)
+{
+    if (PassConstants.NumMeshObjects > dtid.x)
+    {
+        const StructuredBuffer<InstanceData> instances = ResourceDescriptorHeap[Bindings.InstanceBuffer];
+        const InstanceData instance = instances[dtid.x];
+        
+        min16uint meshIndex, materialIndex;
+        UnpackBatchID(instance.BatchID, meshIndex, materialIndex);
+        
+        const StructuredBuffer<Mesh> meshes = ResourceDescriptorHeap[Bindings.MeshBuffer];
+        const Mesh mesh = meshes[meshIndex];
+        
+        const StructuredBuffer<CameraData> cameraBuffer = ResourceDescriptorHeap[Bindings.CameraBuffer];
+        const CameraData camera = cameraBuffer[Bindings.CameraID];
+        
+        const float3 boundsWP = mul(instance.Transform, float4(mesh.Bounds.xyz, 1.f)).xyz;
+        const BoundingSphere bounds = CreateBoundingSphere(boundsWP, mesh.Bounds.w);
+        
+        if (true/*camera.BoundingFrustum.Intersects(bounds)*/)
+        {
+            RWStructuredBuffer<MeshletInstanceIndirectArgs> instancesPassedBuffer = ResourceDescriptorHeap[PassConstants.IndirectArgumentMeshCullingBuffer];
+            RWStructuredBuffer<uint> passedMeshesCounterBuffer = ResourceDescriptorHeap[PassConstants.PassedMeshesCounterBuffer];
+            uint meshObjectIndex;
+            InterlockedAdd(passedMeshesCounterBuffer[0], 1, meshObjectIndex);
+            instancesPassedBuffer[meshObjectIndex].InstanceID = dtid.x;
+            instancesPassedBuffer[meshObjectIndex].GroupsX = ceil(mesh.MeshletCount / (float)THREADS_PER_X); // TODO: Check if OK to divide by 64
+            instancesPassedBuffer[meshObjectIndex].GroupsY = 1;
+            instancesPassedBuffer[meshObjectIndex].GroupsZ = 1;
+        }
+    }
+}
